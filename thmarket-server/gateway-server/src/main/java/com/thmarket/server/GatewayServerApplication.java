766ac82007205c87b3a45393cd6ef094868981bf
沈斌 <bin.shen@ailingua.com.cn>
2019-09-13 15:20:52 +0800

    small fix

--- thmarket-server/gateway-server/src/main/java/com/thmarket/server/GatewayServerApplication.java
+++ thmarket-server/gateway-server/src/main/java/com/thmarket/server/GatewayServerApplication.java
@@ -2,11 +2,18 @@ package com.thmarket.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class GatewayServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServerApplication.class, args);
+    }
+
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
     }
 }
